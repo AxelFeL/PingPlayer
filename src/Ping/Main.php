3e17c5cd2b5d87989a14b4ef6e11b8fd7a79ebcd
AxelFeL542 <91774548+AxelFeL542@users.noreply.github.com>
2021-12-04 09:10:41 +0700

    Main.php

--- src/Ping/Main.php
+++ src/Ping/Main.php
@@ -9,7 +9,7 @@ use pocketmine\command\CommandSender;
 
 class Main extends PluginBase {
     
-    public function onEnable(): void {
+    public function onEnable() : void {
         $this->getLogger()->info("[PP] Plugin Enabled!");
     }
     
